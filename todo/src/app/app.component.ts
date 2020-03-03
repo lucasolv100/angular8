c89aec9b9aa5a2665ea2f754c1c8f0a35f4d358a
Lucas Souza <lucas.souza@plus-it.com.br>
2020-03-03 10:03:50 -0300

    adicionado métodos de edição, exclusão, adição;
    Adicionado formgroups

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: Todo[] = [];
+  public title: string = 'Minhas tarefas';
+  public form: FormGroup
+
+  constructor(private fb: FormBuilder) {
+    
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    })
+
+    this.todos.push(new Todo(1, 'passear com o cachorro', false));
+    this.todos.push(new Todo(2, 'aprender angular', false));
+    this.todos.push(new Todo(3, 'cortar o cabelo', true));
+  }
+
+  add(){
+    const title = this.form.controls['title'].value
+    const id = this.todos.length + 1
+    this.todos.push(new Todo(id, title, false))
+    this.refresh()
+  }
+
+  refresh(){
+    this.form.reset()
+  }
+  
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo)
+    if (index !== -1) {
+      this.todos.splice(index, 1)
+    }
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+  }
+
+  markAsUndone(todo: Todo) {
+    todo.done = false;
+  }
 }
