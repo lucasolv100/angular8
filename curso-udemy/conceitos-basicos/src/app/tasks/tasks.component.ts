e5c976f20529217e0d989b1c8eab0e146005ffbd
Lucas <lucas@smcaetano.com>
2020-04-30 08:28:27 -0300

    adicionado task service e injeção de dependencia

--- curso-udemy/conceitos-basicos/src/app/tasks/tasks.component.ts
+++ curso-udemy/conceitos-basicos/src/app/tasks/tasks.component.ts
@@ -1,35 +1,31 @@
 import { Component, OnInit } from '@angular/core'
 
 import { Task } from './shared/task.model'
+import { TaskService } from './shared/task.service';
 
-const TASKS: Array<Task> = [
-    new Task(1,'Fazer tarefa 1'),
-    new Task(2,'Fazer tarefa 2'),
-    new Task(3,'Fazer tarefa 3'),
-    new Task(4,'Fazer tarefa 4'),
-    new Task(5,'Fazer tarefa 5'),
-    new Task(6,'Fazer tarefa 6'),
-    new Task(7,'Fazer tarefa 7'),
-    new Task(8,'Fazer tarefa 8'),
-];
+
 
 @Component({
     selector: 'tasks',
-    templateUrl: './tasks.component.html'
+    templateUrl: './tasks.component.html',
+    providers: [
+        // { provide: TaskService, useClass: TaskService }
+        TaskService
+    ]
 })
 
 export class TasksComponent implements OnInit {
 
     public tasks: Task[] = [];
-    public selectedTask:Task;
-    constructor() {
+    public selectedTask: Task;
 
+    constructor(private taskService: TaskService) {
     }
     public ngOnInit(): void {
-        this.tasks = TASKS;
+        this.tasks = this.taskService.getTasks();
     }
 
-    public onSelect(task:Task){
+    public onSelect(task: Task) {
         this.selectedTask = task;
     }
 
