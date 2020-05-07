bd6c7e9c4c6419113a08663c677990f11bbcb0a8
Lucas <lucas@smcaetano.com>
2020-05-07 08:11:02 -0300

    adicionado link para acessar detalhes; adicionado link para voltar com location;

--- curso-udemy/conceitos-basicos/src/app/tasks/task-detail/task-detail.component.ts
+++ curso-udemy/conceitos-basicos/src/app/tasks/task-detail/task-detail.component.ts
@@ -6,6 +6,8 @@ import { switchMap } from 'rxjs/operators';
 import { Task } from '../shared/task.model'
 import { TaskService } from '../shared/task.service'
 
+import {Location} from '@angular/common'
+
 @Component({
   selector: 'task-detail',
   templateUrl: './task-detail.component.html',
@@ -13,10 +15,14 @@ import { TaskService } from '../shared/task.service'
 })
 export class TaskDetailComponent implements OnInit {
   public task: Task;
-  constructor(private taskService:TaskService, private activatedRoute: ActivatedRoute) { }
+  constructor(private taskService:TaskService, private activatedRoute: ActivatedRoute, private location: Location) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.pipe(switchMap((params:Params) => this.taskService.getTask(+params['id']))).subscribe(task => this.task = task);
+  }
+
+  goBack(){
+    this.location.back();
   }
 
 }
