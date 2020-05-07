001df31b78aa95148bc1b028255b6d303b6073c7
Lucas <lucas@smcaetano.com>
2020-05-07 09:25:07 -0300

    demais formas de lidar com observable

--- curso-udemy/conceitos-basicos/src/app/learning-observables.service.ts
+++ curso-udemy/conceitos-basicos/src/app/learning-observables.service.ts
@@ -11,6 +11,22 @@ export class LearningObservables {
             complete: () => console.log("Chamou o método COMPLETE"),
         }
 
-        this.http.get("api/tasks").subscribe(observer);
+        //passando um observador como parametro
+        // this.http.get("api/tasks").subscribe(observer);
+
+        //criando um observador como parametro
+        // this.http.get("api/tasks").subscribe({
+        //     next: (newData) => console.log("Chamou o método NEXT e passou como parametro o newData => ", newData),
+        //     error: (errorData) => console.log("Chamou o método ERROR e passou como parametro o errorData => ", errorData),
+        //     complete: () => console.log("Chamou o método COMPLETE"),
+        // });
+
+        //passando diretamente as funções como parametro
+        this.http.get("api/taskaaas").subscribe(
+            (newData) => console.log("Chamou o método NEXT e passou como parametro o newData => ", newData),
+            (errorData) => console.log("Chamou o método ERROR e passou como parametro o errorData => ", errorData),
+            () => console.log("Chamou o método COMPLETE"),
+        );
+
     }
 }
